9424a451b13b63ede6fd80e7811ef189f2752541
Abderahmane <abderahmane.benomari7@gmail.com>
2024-08-02 17:28:26 +0200

    sss
    ss

--- components/Main.js
+++ components/Main.js
@@ -26,7 +26,7 @@ export default function Main() {
       </div>
       <div className="container flex flex-col items-center justify-center py-8 mx-auto rounded-lg md:p-1 p-3">
         <img
-          className="object-cover object-center w-full mb-10 border-gray-200 dark:border-gray-900 g327 border rounded-lg shadow-md"
+          className="object-cover object-center w-full mb-10 border-gray-200 dark:border-gray-900 g327 pr-5 "
           alt="hero"
           src={KebchiImage}
         />
